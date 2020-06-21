34f62f2645eade6d5e306bd39cb1d2341535f4b6
StevOrc <steven.orcun2@gmail.com>
2020-06-22 00:57:23 +0200

    logic implementation for all routes of articles

--- api/routes/article.route.js
+++ api/routes/article.route.js
@@ -17,15 +17,35 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if(error) res.status(400).send(error.details[0].message);
     
+    const {author, category, content} = req.body;
+    let article = new Article({
+        author, category, content
+    });
+    article = await article.save();
+    res.status(200).send(article);
 });
 
 router.put('/:id', async (req, res) => {
+    const {error} = validate(req.body);
+    if(error) res.status(400).send(error.details[0].message);
 
+    const {author, content, category} = req.body;
+    let article = await Article.findByIdAndUpdate(req.params.id, {
+        author, category, content
+    }, {new: true, useFindAndModify: false});
+    
+    if(!article) return res.status(404).send('Article not found with the given ID...');
+    res.status(200).send(article);
 });
 
 router.delete('/:id', async (req, res) => {
+    const article = Article.findByIdAndDelete(req.params.id, {useFindAndModify: false});
+    if(!article) res.status(404).send('No article found with the given ID...');
 
+    res.status(200).send(article);
 });
 
 module.exports = router;
