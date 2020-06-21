3df400531eff46d3611bd5370b04aa4c26e69c63
StevOrc <steven.orcun2@gmail.com>
2020-06-21 21:22:31 +0200

    ok

--- api/routes/article.route.js
+++ api/routes/article.route.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const {Article, validate} = require('../models/article.model');
 
 router.get('/', async (req, res) => {
+    const articles = await Article.find();
+    if(!articles) res.status(404).send('No articles found in DB');
 
+    res.status(200).send(articles);
 });
 
 router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id);
+    if(!article) res.status(404).send('No articles found in DB');
 
+    res.status(200).send(article);
 });
 
 router.post('/', async (req, res) => {
-
+    
 });
 
 router.put('/:id', async (req, res) => {
