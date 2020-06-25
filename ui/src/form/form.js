6f0367353d98d6992fe7b1c712d56fa3b4a746c5
StevOrc <steven.orcun2@gmail.com>
2020-06-25 14:50:29 +0200

    improve method formIsValid, more generic

--- ui/src/form/form.js
+++ ui/src/form/form.js
@@ -2,12 +2,13 @@ import '../assets/styles/styles.scss';
 import './form.scss'
 
 const form = document.querySelector('form');
-const errorElement = document.querySelector("#errors");
+let errorElement = document.querySelector("#errors");
 let errors = [];
 const baseUrl = 'http://localhost:3000/api/articles/';
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
+    errors = [];
     // On passe à l'objet FormData le form
     const formData = new FormData(form);
     // Permet d'avoir un objet KEY - VALUE des différents champs, soit :  {author: '', category: '', content: ''}
@@ -25,25 +26,23 @@ form.addEventListener('submit', async event => {
 
             const body = await response.json();
             console.log(body);
-        }catch(e) {
-            console.log("error : ", e);
+        }catch(error) {
+            console.log(error);
         }
     }
 });
 
 const formIsValid = (article) => {
-    if(!article.author || !article.category || !article.content){
-        errors.push("vous n'avez pas remplis touts les champs");
-        if(errors.length > 1) errors.shift();
-    } else{
-        errors = [];
-    } 
+    for(const [key, value] of Object.entries(article)){
+        console.log("aaaa");
+        if(!value) errors.push(`${key} est obligatoire`);
+    }
     if(errors.length){
-        let errorHtml = errors.reduce( (acc, value) => {
+        const errorHTML = errors.reduce( (acc, value) => {
             acc += `<li>${value}</li>`;
             return acc;
         }, '');
-        errorElement.innerHTML = errorHtml;
+        errorElement.innerHTML = errorHTML;
         return false;
     } else {
         errorElement.innerHTML = '';
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,2 +1,15 @@
 import './assets/styles/styles.scss';
 import './index.scss';
+
+let articles = [];
+const baseUrl = 'http://localhost:3000/api/articles/';
+
+const getAllArticles = async () => {
+    let response = await fetch(baseUrl);
+    console.log(response.json());
+    // response = JSON.stringify(response);
+    // articles = response;
+    // console.log(response);
+}
+
+getAllArticles();
