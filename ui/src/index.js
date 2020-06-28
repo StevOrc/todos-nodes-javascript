e51f5ecd2b8814c3a70f31d1e7bde6d767444d69
StevOrc <steven.orcun2@gmail.com>
2020-06-28 22:14:40 +0200

    Adding JS for index.js => fetch and display all articles... plus some CSS

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,15 +1,42 @@
 import './assets/styles/styles.scss';
 import './index.scss';
 
+const articleContainerElement = document.querySelector('.articles-container');
+const baseURL = 'http://localhost:3000/api/articles/';
 let articles = [];
-const baseUrl = 'http://localhost:3000/api/articles/';
 
-const getAllArticles = async () => {
-    let response = await fetch(baseUrl);
-    console.log(response.json());
-    // response = JSON.stringify(response);
-    // articles = response;
-    // console.log(response);
+const createArticle = () => {
+    const articleNodes = articles.map( article => {
+        return articleNode(article);
+    });
+    articleContainerElement.innerHTML = '';
+    articleContainerElement.append(...articleNodes);
 }
 
-getAllArticles();
+const fetchAllArticles = async () => {
+    try{
+        const response = await fetch(baseURL);
+        const data = await response.json();
+        articles = data;
+        createArticle();
+    }catch(e){
+        console.log(e);
+    }
+}
+
+const articleNode = (article) => {
+    const articleDOM = document.createElement('div');
+    articleDOM.classList.add('article');
+    articleDOM.innerHTML = `
+    <img src="${article.img}" alt="lego-profile">
+    <h2>${article.title}</h2>
+    <p class="article-author">${article.author}</p>
+    <p class="article-content">${article.content}</p>
+    <div class="article-action">
+        <button class="btn btn-danger" data-id="${article._id}">Supprimer</button>
+    </div>
+    `
+    return articleDOM;
+}
+
+fetchAllArticles();
